05c1f55e6af9a0028c1ed225ee393eb4e2e63ba4
ebans1904 <beans2853@gmail.com>
2024-06-19 23:17:30 +0700

    Update BpmnTest.js
    keyboard shortcut & css updates

--- client/src/publish/BpmnTest.js
+++ client/src/publish/BpmnTest.js
@@ -6,15 +6,16 @@ import 'bpmn-js/dist/assets/bpmn-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
 
 
-function BpmnTest(){
+function BpmnTest() {
     const container = useRef(null);
-    const [ modeler, setModeler ] = useState(null);
+    const [modeler, setModeler] = useState(null);
     let modelerInstance = null;
 
     useEffect(() => {
-        if(modelerInstance) return;
+        if (modelerInstance) return;
         modelerInstance = new BpmnModeler({
             container: container.current,
+            keyboard: {bindTo: container.current},
             additionalModules: [
                 minimapModule
             ]
@@ -34,9 +35,14 @@ function BpmnTest(){
         }
     }, []);
 
-    return(
-        <div className='modeler-parent'>
-            <div id='modeler-container' ref={container} style={{width: "100%", height: "100%"}}></div>
+    return (
+        <div className='main-container'>
+            <div className='modeler-header'>
+
+            </div>
+            <div id='modeler-container' ref={container}>
+                <img src="https://svgshare.com/i/eGa.svg" className='grid-bg'/>
+            </div>
         </div>
     )
 }

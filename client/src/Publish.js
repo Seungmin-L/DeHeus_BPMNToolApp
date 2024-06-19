23e53f97c97e32dcafb33a0886eb516e470d1935
Chris0217 <102713143+Chris0217@users.noreply.github.com>
2024-06-19 15:53:42 +0700

    Set Error Route

--- client/src/Publish.js
+++ client/src/Publish.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Error from './Error';
 
 function Publish() {
   const { filename } = useParams();
-  const Component = React.lazy(() => import(`./publish/${filename}`));
+  const [Component, setComponent] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const importFile = async () => {
+      try {
+        const ImportedComponent = await import(`./publish/${filename}`);
+        setComponent(() => ImportedComponent.default);
+      } catch (err) {
+        setError(true);
+      }
+    };
+
+    importFile();
+  }, [filename]);
+
+
+  if (error) {
+    return <Error />;
+  }
+
+  if (!Component) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <Component />
-    </React.Suspense>
+    <Component />
   );
 }
 

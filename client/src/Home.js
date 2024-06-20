a31d4d30e32e089414f051639377743f76cbb1a2
ChristinaYoo <christinaa@cj.net>
2024-06-20 12:34:02 +0700

    Add Bootstrap&Icon to Home and Error

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { FaHome } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
+  const goLogin = () => {
+    navigate("/login");
+  };
+
   return (
-    <div>This is Home Page</div>
-  )
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <FaHome size={100} className="mb-4 text-primary" />
+      <h1 className="display-1">De Heus BPMN Tool Application</h1>
+      <p className="lead">
+        This is the landing page. (Design to be edited further.)
+      </p>
+      <Button variant="primary" onClick={goLogin} size="lg" className="mt-4">
+        Start
+      </Button>
+    </div>
+  );
 }
 
 export default Home;

bec72767e4bc5f7879f172f7be04e0884fd7d2c5
Chris0217 <102713143+Chris0217@users.noreply.github.com>
2024-06-28 17:21:43 +0700

    Set Home design

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
 import { useMsal, useIsAuthenticated } from "@azure/msal-react";
 import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
-import { FaHome } from "react-icons/fa";
 import { loginRequest } from "../config/authConfig";
 import { useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/backgrounds/home_background.png";
+import logo from "../assets/logos/logo_deheus.png";
 
 function Home() {
   const { instance } = useMsal();
@@ -24,20 +25,23 @@ function Home() {
   };
 
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
-      <FaHome size={100} className="mb-4 text-primary" />
-      <h1 className="display-1">De Heus BPMN Tool Application</h1>
-      <p className="lead">
-        This is the landing page. (Design to be edited further.)
-      </p>
-      <Button
-        variant="primary"
-        onClick={handleLogin}
-        size="lg"
-        className="mt-4"
-      >
-        Start
-      </Button>
+    // <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+    <div className="d-flex flex-column justify-content-center align-items-start vh-100" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh' }}>
+      <div style={{ marginLeft: "15%", color: "#004085" }}>
+        <img src={logo} alt="De Heus Logo" style={{ width: "120px", marginBottom: "25px" }} /> {/* Logo Image */}
+        <h1 className="display-1 mb-3">De Heus<span className="d-block">BPMN Tool</span></h1>
+        <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<span className="d-block">tempor incididunt ut labore et dolore magna aliqua.</span>
+        </p>
+        <Button
+          onClick={handleLogin}
+          size="lg"
+          className="mt-4"
+          style={{ backgroundColor: "#2A85E2", width: "30%"}}
+        >
+          Start
+        </Button>
+      </div>
     </div>
   );
 }

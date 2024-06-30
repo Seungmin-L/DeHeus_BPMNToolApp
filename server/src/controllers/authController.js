2d29fda2a50440b2baf7b5cf2e7a1d494356ef2e
Seungmin-L <s3864235@rmit.edu.vn>
2024-06-30 23:26:28 +0700

    Update authController.js

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -2,16 +2,16 @@ const jwt = require('jsonwebtoken');
 const sql = require('mssql');
 
 const authenticateUser = async (req, res) => {
-  console.log("Received request:", req.body); // 추가된 로그
+  console.log("Received request:", req.body);
   const { token } = req.body;
 
   try {
     // 토큰 검증 및 디코딩
     const decodedToken = jwt.decode(token, { complete: true });
-    console.log(decodedToken); // 터미널에 토큰 정보 출력
+    console.log(decodedToken); // 터미널에 토큰 정보 출력 (확인 용도)
 
     // 디코딩된 토큰에서 이메일 추출
-    const email = decodedToken.payload.preferred_username;
+    const email = decodedToken.payload.email;
     console.log(`User Email: ${email}`);
 
     const userResult = await sql.query`SELECT * FROM Users WHERE email = ${email}`;
@@ -20,7 +20,7 @@ const authenticateUser = async (req, res) => {
       return res.status(401).json({ message: 'User not found in the database' });
     }
 
-    // 사용자 정보 업데이트
+    // 데이터베이스에 사용자 정보 업데이트
     const userId = userResult.recordset[0].id;
     await sql.query`
       UPDATE Users 
@@ -30,7 +30,7 @@ const authenticateUser = async (req, res) => {
         tenant_id = ${decodedToken.payload.tid},
         token_issue_time = ${decodedToken.payload.iat},
         token_expiration_time = ${decodedToken.payload.exp},
-        nonce = ${decodedToken.payload.nonce},
+        nonce = ${decodedToken.header.nonce},
         identity_provider = ${decodedToken.payload.idp},
         token_id = ${decodedToken.payload.uti},
         resource_id = ${decodedToken.payload.aud}

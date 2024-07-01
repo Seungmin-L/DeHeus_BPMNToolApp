1d3744a7749f727ce7f17d9e7123ac89835ab331
Seungmin-L <s3864235@rmit.edu.vn>
2024-07-01 18:45:56 +0700

    Update Home.js
    User Verification, Token, etc

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,68 +1,91 @@
-import { useMsal } from "@azure/msal-react";
-// import { useMsal, useIsAuthenticated } from "@azure/msal-react";
-import React, { useState } from "react";
+import { useMsal, useIsAuthenticated, useAccount } from "@azure/msal-react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import backgroundImage from "../assets/backgrounds/home_background.png";
 import logo from "../assets/logos/logo_deheus.png";
 import { loginRequest } from "../config/authConfig";
 import axios from 'axios';
 
 function Home() {
-  const { instance } = useMsal();
+  const { instance, accounts } = useMsal();
+  const account = useAccount(accounts[0]);
   const [loginError, setLoginError] = useState('');
-  // const isAuthenticated = useIsAuthenticated();
-  // const navigate = useNavigate();
+  const isAuthenticated = useIsAuthenticated();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   if (isAuthenticated) {
-  //     navigate("/main");
-  //   }
-  // }, [isAuthenticated, navigate]);
-
-  const handleLoginRedirect = () => {
-    console.log('[handleLoginRedirect] Handling login redirect');
-    instance.loginPopup(loginRequest).then(
-      async (loginResponse) => {
-        const accessToken = loginResponse.accessToken;
-        console.log(`[handleLoginRedirect] Logged in user token: ${accessToken}`);
-
-        // 백엔드로 토큰 전달
-        try {
-          const response = await axios.post('http://localhost:3001/api/authenticate', {
-            token: accessToken,
-          }, {
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          });
-        if (response.status === 200 && response.data.isAuthenticated) {
-          console.log('[handleLoginPopup] Email is registered, proceeding with login');
-          window.location.href = "http://localhost:3000/main"; // 리다이렉트 링크
+  const verifyUserRegistration = async (accessToken) => {
+    try {
+      const response = await axios.post('http://localhost:3001/api/authenticate', {
+        token: accessToken,
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
         }
-      } catch (error) {
-        if (error.response.status === 401) {
-          console.log('[handleLoginPopup] Email is not registered, logging out');
-          setLoginError('Email is not registered.');
-        } else {
-          console.error(`[handleLoginPopup] Server error: ${error}`);
-          setLoginError('Server error occurred.');
-        }
+      });
+      if (response.status === 200 && response.data.isAuthenticated) {
+        navigate("/main");
+      } else {
+        setLoginError('Email is not registered.');
+        handleLogout();
       }
-    }).catch((error) => {
-      console.error(`[handleLoginRedirect] Login error: ${error}`);
+    } catch (error) {
+      console.error('Server error:', error);
+      setLoginError('Server error occurred.');
+    }
+  };
+
+  useEffect(() => {
+    if (account && localStorage.getItem('msalAccount')) {
+        verifyUserRegistration(localStorage.getItem('msalToken'));
+    }
+  }, [account, isAuthenticated, accounts, verifyUserRegistration]);
+
+  const handleLoginRedirect = async () => {
+    if (!accounts.length) {
+      try {
+        const loginResponse = await instance.loginPopup(loginRequest);
+        instance.setActiveAccount(loginResponse.account);
+        localStorage.setItem('msalAccount', loginResponse.account.username);
+        await acquireToken(loginResponse.account);
+      } catch (error) {
+        console.error('Login popup error:', error);
+        setLoginError('Login failed with popup.');
+      }
+    } else {
+      await acquireToken(account);
+    }
+  };
+
+  const acquireToken = async (account) => {
+    try {
+      const loginResponse = await instance.acquireTokenSilent({
+        ...loginRequest,
+        account: account
+      });
+      localStorage.setItem('msalToken', loginResponse.accessToken);
+      await verifyUserRegistration(loginResponse.accessToken);
+    } catch (error) {
+      console.error('Error acquiring token:', error);
       setLoginError('Login failed.');
-    });
+      localStorage.removeItem('msalToken');
+      localStorage.removeItem('msalAccount');
+    }
+  };
+
+  const handleLogout = () => {
+    instance.logout();
+    localStorage.removeItem('msalToken');
+    localStorage.removeItem('msalAccount');
+    navigate("/login");
   };
 
   return (
-    // <div className="d-flex flex-column justify-content-center align-items-center vh-100">
     <div className="d-flex flex-column justify-content-center align-items-start vh-100" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh' }}>
       <div style={{ marginLeft: "15%", color: "#004085" }}>
-        <img src={logo} alt="De Heus Logo" style={{ width: "120px", marginBottom: "25px" }} /> {/* Logo Image */}
+        <img src={logo} alt="De Heus Logo" style={{ width: "120px", marginBottom: "25px" }} />
         <h1 className="display-1 mb-3">De Heus<span className="d-block">BPMN Tool</span></h1>
         <p className="lead">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<span className="d-block">tempor incididunt ut labore et dolore magna aliqua.</span>
         </p>
         <Button
           onClick={handleLoginRedirect}

055fd7582eca492d0e4daaa2075e3e188f83f326
numi8462 <yhk8462@naver.com>
2024-07-02 19:33:21 +0900

    file update

--- client/src/publish/bpmn.js
+++ client/src/publish/bpmn.js
@@ -1,119 +1,193 @@
 import React, { useEffect, useRef, useState } from 'react';
-import BpmnJS from 'bpmn-js/lib/Modeler';
+
+// BPMN imports
+import BpmnModeler from 'bpmn-js/lib/Modeler';
 import minimapModule from 'diagram-js-minimap';
-import $ from 'jquery';
-import { 
-  BpmnPropertiesPanelModule, 
-  BpmnPropertiesProviderModule,  
-  ZeebePropertiesProviderModule, 
-  CamundaPlatformPropertiesProviderModule // Camunda 8 provider
-} from 'bpmn-js-properties-panel';
-import zeebeModdle from 'zeebe-bpmn-moddle/resources/zeebe';
-import ZeebeBehaviorsModule from 'camunda-bpmn-js-behaviors/lib/camunda-cloud';
-import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda'; // Import this
-
-
-import 'bpmn-js/dist/assets/bpmn-js.css';
+import ColorPickerModule from 'bpmn-js-color-picker';
 import 'bpmn-js/dist/assets/diagram-js.css';
-import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
+import 'bpmn-js/dist/assets/bpmn-js.css';
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
-import '../property-panel.css';
+import { 
+    BpmnPropertiesPanelModule, 
+    BpmnPropertiesProviderModule,  
+  } from 'bpmn-js-properties-panel';
 
+// diagram file
 import diagramXML from '../resources/pizza-collaboration.bpmn';
 
+// toolbar
+import Toolbar from './features/toolbar/toolbar';
 
-const BPMN = () => {
-  const container = useRef(null);
-  const [modeler, setModeler] = useState(null);
-  const [isHidden, setIsHidden] = useState(false);
-  let modelerInstance = null;
-  const diagramUrl = 'https://cdn.statically.io/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
 
-  useEffect(() => {
-    if (modelerInstance) return;
-    modelerInstance = new BpmnJS({
-      container: container.current,
-      keyboard: {
-        bindTo: document
-      },
-      propertiesPanel: {
-        parent: '#properties-panel-parent'
-      },
-      additionalModules: [
-          BpmnPropertiesPanelModule,
-          BpmnPropertiesProviderModule,
-          CamundaPlatformPropertiesProviderModule,
-          minimapModule,
-          // ZeebePropertiesProviderModule,
-          // ZeebeBehaviorsModule,
-      ],
-      moddleExtensions: {
-        camunda: camundaModdleDescriptor 
-        // zeebe: zeebeModdle
-      }
-    });
+function BpmnTest() {
+    const diagramUrl = 'https://cdn.statically.io/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn';
 
-    // const exportDiagram = async () => {
-    //   try {
-    //     const result = await bpmnModeler.saveXML({ format: true });
-    //     alert('Diagram exported. Check the developer tools!');
-    //     console.log('DIAGRAM', result.xml);
-    //   } catch (err) {
-    //     console.error('could not save BPMN 2.0 diagram', err);
-    //   }
-    // };
+    const container = useRef(null);
+    let [modelerInstance, setModeler] = useState(null); // <-- Add this line
+    // Event handler to toggle the export dropdown
 
-    // opens diagram
-    const openDiagram = async (bpmnXML) => {
-      try {
-        await modelerInstance.importXML(bpmnXML);
-        const canvas = modelerInstance.get('canvas');
-        const overlays = modelerInstance.get('overlays');
-        canvas.zoom('fit-viewport');
-        overlays.add('SCAN_OK', 'note', {
-          position: {
-            bottom: 0,
-            right: 0
-          },
-          html: '<div class="diagram-note">Mixed up the labels?</div>'
+
+    useEffect(() => {
+        if (modelerInstance) return;
+        modelerInstance = new BpmnModeler({
+            container: container.current,
+            keyboard: { bindTo: document },
+            propertiesPanel: {
+                parent: '#properties-panel-parent'
+            },
+            additionalModules: [
+                BpmnPropertiesPanelModule,
+                BpmnPropertiesProviderModule,
+                ColorPickerModule,
+            ],
+            moddleExtensions: {
+            },
+            textRenderer: {
+                defaultStyle: {
+                //   fontFamily: 'none'
+                }
+            },
+            bpmnRenderer: {
+                defaultStrokeColor: '#333'
+            }
         });
-        canvas.addMarker('SCAN_OK', 'needs-discussion');
-      } catch (err) {
-        console.error('could not import BPMN 2.0 diagram', err);
-      }
+
+        // opens diagram
+        const openDiagram = async (bpmnXML) => {
+            try {
+            await modelerInstance.importXML(bpmnXML);
+            const canvas = modelerInstance.get('canvas');
+            const overlays = modelerInstance.get('overlays');
+            canvas.zoom('fit-viewport');
+            overlays.add('SCAN_OK', 'note', {
+                position: {
+                bottom: 0,
+                right: 0
+                },
+                html: '<div class="diagram-note">Mixed up the labels?</div>'
+            });
+            canvas.addMarker('SCAN_OK', 'needs-discussion');
+            } catch (err) {
+            console.error('could not import BPMN 2.0 diagram', err);
+            }
+        };
+    
+        fetch('https://cdn.statically.io/gh/bpmn-io/bpmn-js-examples/dfceecba/starter/diagram.bpmn')
+            .then(response => response.text())
+            .then(openDiagram);
+
+        // modelerInstance.createDiagram().then(() => {
+        //     modelerInstance.get('keyboard').bind(document);
+        //     console.log(modelerInstance.get('commandStack')); // Should log the commandStack
+        // });
+        setModeler(modelerInstance);
+        // return () => {
+        //     modeler?.destroy();
+        // }
+        console.log(modelerInstance)
+    }, []);
+
+    const handleZoomIn = () => {
+        modelerInstance?.get('zoomScroll').stepZoom(1);
     };
 
-    modelerInstance.createDiagram().then(() => {
-      modelerInstance.get('keyboard').bind(document);
-    });
-    setModeler(modelerInstance);
-    return () => {
-        modeler?.destroy();
+    const handleZoomOut = () => {
+        modelerInstance?.get('zoomScroll').stepZoom(-1);
+    };
+
+    const handleUndo = () => {
+        modelerInstance?.get('commandStack').undo();
+        console.log(modelerInstance?.get('commandStack'))
+    };
+
+    const handleRedo = () => {
+        modelerInstance?.get('commandStack').redo();
+    };
+
+    const handleSave = () => {
+    // Implement save functionality here
+    };
+
+    const handleImport = () => {
+        // import function
     }
-    // $.get(diagramUrl, openDiagram, 'text');
 
+    const handleExport = () => {
+        // export function
 
-  }, []);
+    }
 
-  const handleHidden = () => {
-    setIsHidden(prev => !prev);
-  }
+    const handleAlign = (alignment) => {
+        const alignElements = modelerInstance.get('alignElements');
+        const selection = modelerInstance.get('selection');
+        const selectedElements = selection.get();
+    
+        if (selectedElements.length > 1) {
+            alignElements.trigger(selectedElements, alignment);
+        } else {
+            console.log('Please select at least two elements to align.');
+        }
+    };
+    
+    const handleDistribute = (direction) => {
+        const distributeElements = modelerInstance.get('distributeElements');
+        const selection = modelerInstance.get('selection');
+        const selectedElements = selection.get();
+    
+        if (selectedElements.length > 2) {
+            distributeElements.trigger(selectedElements, direction);
+        } else {
+            console.log('Please select at least three elements to distribute.');
+        }
+    };
+      
+    const handleChangeColor = () => {
+        const selection = modelerInstance.get('selection');
+        const modeling = modelerInstance.get('modeling');
+        const selectedElement = selection.get()[0]; // get the first selected element
+      
+        if (selectedElement) {
+          modeling.setColor(selectedElement, {
+            stroke: 'red',
+            
+          });
+        //   console.log(selectedElement);
+        }
+    };
+      
 
-  return (
-    <div className='main-container'>
+    return (
+        <div className='main-container'>
 
-      <div className='model-header'></div>
+            <div className='model-header'>
+            <Toolbar 
+                onSave={handleSave} 
+                onImport={handleImport}
+                onExport={handleExport}
+                onZoomIn={handleZoomIn} 
+                onZoomOut={handleZoomOut} 
+                onUndo={handleUndo} 
+                onRedo={handleRedo} 
+                onAlignLeft={() => handleAlign('left')} 
+                onAlignCenter={() => handleAlign('center')} 
+                onAlignRight={() => handleAlign('right')} 
+                onAlignTop={() => handleAlign('top')} 
+                onAlignMiddle={() => handleAlign('middle')} 
+                onAlignBottom={() => handleAlign('bottom')} 
+                onDistributeHorizontally={() => handleDistribute('horizontal')} 
+                onDistributeVertically={() => handleDistribute('vertical')} 
+            />
+            {/* <button onClick={handleChangeColor}>Change Color</button> */}
 
-      <div className='model-body'>
-        <div className={'hierarchy-sidebar ' + (isHidden ? "hide" : "")}>
-            <button onClick={handleHidden} style={{width: "50px"}}>{isHidden ? "Show" : "Hide"}</button>
+            </div>
+
+            <div className='model-body'>
+                <div id='modeler-container' ref={container}/>
+                <div id='properties-panel-parent' />
+            </div>
+
         </div>
-        <div id='modeler-container' className={"" + (isHidden ? 'sidebar-hidden' : '')} ref={container}/>
-        <div id='properties-panel-parent'></div>
-      </div>
+    )
+}
 
-    </div>
-  );
-};
-
-export default BPMN;
+export default BpmnTest;

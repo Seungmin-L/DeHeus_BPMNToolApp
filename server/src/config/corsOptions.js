4ca309828116df32b7aa0976ed6189770ffb56a8
Seungmin-L <s3864235@rmit.edu.vn>
2024-07-22 17:15:39 +0700

    cors config update

--- server/src/config/corsOptions.js
+++ server/src/config/corsOptions.js
@@ -1,11 +1,13 @@
-const whitelist = ['http://localhost:3000'];  // 추후 배포 환경 링크도 추가할 것
+const whitelist = ['http://localhost:3000', 'http://localhost:3001'];  // 추후 배포 환경 링크도 추가할 것
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
+    if (whitelist.includes(origin) || !origin) {
+      // console.log(`Allowed CORS for ${origin}`);
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'), false);
+      // console.warn(`Blocked CORS for ${origin}`);
+      callback(new Error('Not allowed by CORS policy'), false);  // 정보 노출 최소화
     }
   },
   credentials: true,

e01070ec3c8ca95c13f99092e2577eaa3671f693
ebans1904 <beans2853@gmail.com>
2024-08-14 00:05:50 +0700

    Enable attachments in all elements

--- client/src/providers/AttachmentPropertiesProvider.js
+++ client/src/providers/AttachmentPropertiesProvider.js
@@ -34,7 +34,7 @@ export default function AttachmentPropertiesProvider(propertiesPanel, translate)
     return function(groups) {
 
       // Add the "attachment" group
-      if (is(element, 'bpmn:Task')) {
+      if (is(element, 'bpmn:BaseElement')) {
         groups.push(createAttachGroup(element, translate));
       }
 

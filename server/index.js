ab0d10238991cbf5dfced77cc7ccd2f6f2d727fb
Seungmin-L <s3864235@rmit.edu.vn>
2024-08-24 03:38:11 +0700

    functions for admin
    add, remove, and change the project role & access

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const projectsController = require('./src/controllers/projectsController');
 const processesController = require('./src/controllers/processesController');
 const diagramController = require('./src/controllers/diagramController');
 const attachmentsController = require('./src/controllers/attachmentsController');
+const userController = require('./src/controllers/userController');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,11 +25,17 @@ app.post('/api/diagram/save', diagramController.draftSave);
 
 app.get('/api/projects', projectsController.listProjects);
 app.get('/api/processes/:projectId', processesController.listProcesses);
-app.get('/api/diagrams/get-diagram-with-project/:projectId/:diagramId', diagramController.getDiagramData);
 
+app.get('/api/diagrams/get-diagram-with-project/:projectId/:diagramId', diagramController.getDiagramData);
 app.get('/api/attachments/:diagramId/:nodeId/:fileName', attachmentsController.getAttachment);
 app.post('/api/attachments/:diagramId', attachmentsController.addAttachments);
 app.post('/api/attachments/:diagramId/:nodeId/:fileName', attachmentsController.deleteAttachments);
+
+app.get('/api/admin/users', userController.getUserList)
+app.get('/api/admin/users/:identifier', userController.getUserData);
+
+// app.post('/api/admin/update-role', userController.updateRole);
+app.post('/api/admin/save-user-data', userController.saveUserData);
 
 app.get('/', (req, res) => {
   res.send('Welcome to the backend server!');
